ac8b1de2a3a2387c7eb570815dec4fb05c83ea13
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-05-04 18:07:11 +0600

    active

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -3,10 +3,11 @@ import './Header.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from '../../../assets/spatula-crossed-with-chef-hat-on-top-free-vector.png'
 import { useContext } from 'react';
 import { AuthContext } from '../../../Providers/AuthProviders';
+import ActiveRoute from '../ActiveRoute/ActiveRoute';
 
 
 const Header = () => {
@@ -22,28 +23,27 @@ const Header = () => {
     return (
         <div className='header'
         >
-            <Navbar bg='warning'  expand="lg">
+            <Navbar bg='warning' expand="lg">
                 <Container>
                     <Navbar.Brand ><img style={{ height: '40px' }} src={logo} />  <span className='text-dark'>Mr Chef</span></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            
-                            <Link className='link text-dark' to="/">Home</Link>
-                            <Link className='link text-dark' to="/blog">Blog</Link>
+                            <ActiveRoute to="/">Home</ActiveRoute>
+                            <ActiveRoute className='link-dark' to="/blog">Blog</ActiveRoute>
                             {
                                 user ?
                                     <div>
                                         {user && <img className='profilePicture rounded-circle'
                                             src={user.photoURL} alt={user.displayName}
-                                            title={user.displayName } />}
+                                            title={user.displayName} />}
 
                                         <Link
                                             onClick={handleLogOut}
-                                            className='link text-dark' >LogOut</Link>
+                                            className='link-dark' >LogOut</Link>
                                     </div>
                                     :
-                                    <Link className='link text-dark' to="/login">Login</Link>
+                                    <ActiveRoute className='link-dark' to="/login">Login</ActiveRoute>
                             }
 
                         </Nav>
